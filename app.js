fdd86e01bc50139fe9638edc7eaf160f81717213
Aleksey Kuzmenko <kurtzpb@gmail.com>
2018-01-18 21:11:13 +0200

    In between dev commit

--- app.js
+++ app.js
@@ -2,10 +2,4 @@
 const fs = require('fs');
 const _ = require('lodash');
 
-console.log(_.uniq([1, 2, 1, 5, 3, 5, 7, 23, 7, 32, 23]));
-
-// fs.appendFile('temp.js', 'module.exports.helloUser = () => {const os = require("os"); return os.userInfo().username};', () => {
-//     const temp = require('./temp');
-//     console.log(temp)
-//     fs.unlinkSync('./temp.js');
-// });
+const temp = require('./notes');
